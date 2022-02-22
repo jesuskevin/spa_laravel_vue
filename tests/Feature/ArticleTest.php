10170d19fc27e29c7c235277ac200bad87b761e8
Kevin <de.jesuskevin01@hotmail.com>
2022-02-22 18:15:41 -0400

    Tests for the article model

---
+++ tests/Feature/ArticleTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Comment;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleTest extends TestCase
+{
+    use LazilyRefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_a_collection_of_articles()
+    {
+        $user = User::factory()->create();
+        $article = Article::factory()->create();
+
+        $this->json('GET', '/api/articles')
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => [
+                    [
+                        'id' => $article->id,
+                        'attributes' => [
+                            'title' => $article->title,
+                            'content' => $article->content,
+                            'picture' => $article->thumbnail,
+                            'created_at' => $article->created_at->diffForHumans()
+                        ],
+                    ]
+                ],
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_an_article()
+    {
+        $user = User::factory()->create();
+        $article = Article::factory()->create();
+
+        $this->json('GET', "/api/articles/{$article->slug}")
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'id' => $article->id,
+                'attributes' => [
+                    'title' => $article->title,
+                    'content' => $article->content,
+                    'picture' => $article->thumbnail,
+                    'created_at' => $article->created_at->diffForHumans()
+                ],
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_an_article()
+    {
+        $user = User::factory()->create();
+
+        $data = [
+            'title' => 'This is the title',
+            'thumbnail' => 'https://picsum.photos/250/200',
+            'content' => 'This is the contetn',
+            'user_id' => $user->id,
+        ];
+
+        $this->json('POST', '/api/articles', $data)
+            ->assertStatus(Response::HTTP_CREATED);
+
+        $this->assertEquals(1, Article::count());
+    }
+
+    public function it_deletes_an_article()
+    {
+        $article = Article::factory()->create();
+
+        $this->json('DELETE', "/api/articles/{$article->slug}")
+            ->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertNull(Article::find($article->id));
+    }
+}
